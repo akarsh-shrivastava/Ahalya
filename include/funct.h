6cb7302bcfc6b1243e73bc8fd53293295654395c
akarsh-shrivastava <akarsh.mails@rediffmail.com>
2018-11-19 01:46:20 +0530

    added frequently used functions

---
+++ include/funct.h
@@ -0,0 +1,6 @@
+#ifndef FUNCT_H_INCLUDED
+    #define FUNCT_H_INCLUDED
+    void print_binary(int number);
+    int getch();
+    int kbhit(void);
+#endif
---
+++ src/funct.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include "funct.h"
+
+#if defined(__linux__) || defined(__APPLE__)
+#include <fcntl.h>
+#include <termios.h>
+#define STDIN_FILENO 0
+#elif defined(_WIN32) || defined(_WIN64)
+#include <conio.h>
+#endif
+
+void print_binary(int number)
+{
+    if (number) {
+        print_binary(number >> 1);
+        putc((number & 1) ? '1' : '0', stdout);
+    }
+}
+
+int getch()
+{
+    #if defined(__linux__) || defined(__APPLE__)
+        struct termios oldt, newt;
+        int ch;
+        tcgetattr(STDIN_FILENO, &oldt);
+        newt = oldt;
+        newt.c_lflag &= ~(ICANON | ECHO);
+        tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+        ch = getchar();
+        tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+        return ch;
+    #elif defined(_WIN32) || defined(_WIN64)
+        return _getch();
+    #endif
+}
+
+int kbhit(void)
+{
+    #if defined(__linux__) || defined(__APPLE__)
+        struct termios oldt, newt;
+        int ch;
+        int oldf;
+
+        tcgetattr(STDIN_FILENO, &oldt);
+        newt = oldt;
+        newt.c_lflag &= ~(ICANON | ECHO);
+        tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+        oldf = fcntl(STDIN_FILENO, F_GETFL, 0);
+        fcntl(STDIN_FILENO, F_SETFL, oldf | O_NONBLOCK);
+
+        ch = getchar();
+
+        tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+        fcntl(STDIN_FILENO, F_SETFL, oldf);
+
+        if (ch != EOF)
+        {
+            ungetc(ch, stdin);
+            return 1;
+        }
+
+        return 0;
+    #elif defined(_WIN32) || defined(_WIN64)
+        return _kbhit();
+    #endif
+}
