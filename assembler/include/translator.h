35c5b658aefe16810bfa9e9caee472d2c47680b4
akarsh-shrivastava <akarsh.mails@rediffmail.com>
2018-11-30 01:59:41 +0530

    syntax decided

--- assembler/include/translator.h
+++ assembler/include/translator.h
@@ -1,11 +1,13 @@
-##ifndef TRANSLATOR_H_INCLUDED
-    #define TRANSLATOR_H_INCLUDED value
+#ifndef TRANSLATOR_H_INCLUDED
+    #define TRANSLATOR_H_INCLUDED
     #include <vector>
+    #include "analyzer.h"
     #include "structs.h"
 
-    struct Translator
+    struct Translator: public Analyzer
     {
         std::vector<Token_struct> token_data;
+        std::string decimal_code;
 
         Translator(std::vector<Token_struct> data);
     }
--- assembler/src/translator.cpp
+++ assembler/src/translator.cpp
@@ -1,19 +1,31 @@
 #include <vector>
 #include <string>
 #include <iterator>
+#include <stack>
+#include <queue>
 #include "translator.h"
 #include "structs.h"
 
-Translator::Translator(std::vector<Token_struct> data)
+Translator::Translator(std::vector<Token_struct> data) : Analyzer()
 {
-	token_data = data;
+    token_data   = data;
+    decimal_code = "";
 }
 
 void Translator::build_decimal_instr()
 {
-	std::vector<Token_struct>::iterator itr;
-	for (itr = token_data.begin(); itr != token_data.end(); ++itr)
-	{
-		
-	}
+    std::stack<std::string> status;
+    std::vector<Token_struct>::iterator itr;
+    for (itr = token_data.begin(); itr != token_data.end(); ++itr)
+    {
+        if (status.empty())
+        {
+            if (itr->type == "instruction_1addr");
+            else if (itr->type == "instruction_2addr");
+            else if (itr->type == "jump");
+            else if (itr->type == "possible_identifier");
+            else;
+            
+        }
+    }
 }
